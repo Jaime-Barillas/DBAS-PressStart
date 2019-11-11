e3dace825a83f1c7e6022ad429ef6210fa40b22c
Shaun <shaun.k.mccrum@gmail.com>
2019-11-11 13:34:29 -0500

    Connection troubleshooting

--- db/setup.js
+++ db/setup.js
@@ -13,7 +13,6 @@ function setupDatabase() {
         user: 'pressstartadmin',
         database: 'postgres'      // The database may not yet exist so we log into the postgres database in the meantime.
     });
-
     client.connect();
     return client.query('DROP DATABASE IF EXISTS pressstartdb;')
                  .then(() => client.query('CREATE DATABASE pressstartdb;'))
@@ -24,34 +23,35 @@ function setupTables() {
     let client = new Client({
         user: 'pressstartadmin',
         database: 'pressstartdb'
+        
     });
 
     // TODO: Maybe move sql to its own file and read it in?
     client.connect();
     return client.query(`CREATE TABLE tbl_members(
-                     member_id serial PRIMARY KEY,
-                     member_password varchar(512),
-                     member_preffered_store integer,
-                     member_first_name varchar(20),
-                     member_last_name varchar(30),
-                     member_postal_code varchar(6),
-                     member_phone varchar(10),
-                     member_email varchar(60),
-                     member_mailing_list boolean
-                 );`)
-                 .then(() => client.query(`CREATE TABLE tbl_items(
-                     item_id serial,
-                     item_type_id integer,
-                     item_store_id integer,
-                     item_condition_id integer,
-                     item_name varchar(20),
-                     item_cost money,
-                     item_sale_price money,
-                     item_mrsp money,
-                     item_stock_quantity smallint,
-                     item_description varchar(120)
-                 );`))
-                 .then(() => client.end());
+                    member_id serial PRIMARY KEY,
+                    member_password varchar(512),
+                    member_preffered_store integer,
+                    member_first_name varchar(20),
+                    member_last_name varchar(30),
+                    member_postal_code varchar(6),
+                    member_phone varchar(10),
+                    member_email varchar(60),
+                    member_mailing_list boolean
+                );`)
+                .then(() => client.query(`CREATE TABLE tbl_items(
+                    item_id serial,
+                    item_type_id integer,
+                    item_store_id integer,
+                    item_condition_id integer,
+                    item_name varchar(20),
+                    item_cost money,
+                    item_sale_price money,
+                    item_mrsp money,
+                    item_stock_quantity smallint,
+                    item_description varchar(120)
+                );`))
+                .then(() => client.end());
 }
 
 function randPrice(max) {
