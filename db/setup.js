bf3922f400d97ba078b0f8a3a8670c1980ffa5fe
Shaun McCrum <shaun.k.mccrum@gmail.com>
2019-11-18 12:24:10 -0500

    Update setup generation
    Added Employee table generation

--- db/setup.js
+++ db/setup.js
@@ -11,8 +11,9 @@ const path = require('path');
 const { Client } = require('pg');
 
 /*======= Variables =======*/
-const firstNames = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'firstnames-short.json'), 'utf8'));
-const lastNames = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'lastnames-short.json'), 'utf8'));
+const firstNames = JSON.parse(fs.readFileSync(path.resolve(__dirname/population_scripts, 'firstnames-short.json'), 'utf8'));
+const lastNames = JSON.parse(fs.readFileSync(path.resolve(__dirname/population_scripts, 'lastnames-short.json'), 'utf8'));
+const streetnames = JSON.parse(fs.readFileSync(path.resolve(__dirname/population_scripts, 'streetnames.json'), 'utf8'));
 
 /*======= Functions =======*/
 function setupDatabase() {
@@ -36,28 +37,41 @@ function setupTables() {
     // TODO: Maybe move sql to its own file and read it in?
     client.connect();
     return client.query(`CREATE TABLE tbl_members(
-                    member_id serial PRIMARY KEY,
-                    member_password varchar(512),
-                    member_preffered_store integer,
-                    member_first_name varchar(20),
-                    member_last_name varchar(30),
-                    member_postal_code varchar(6),
-                    member_phone varchar(10),
-                    member_email varchar(60),
-                    member_mailing_list boolean
-                );`)
-                .then(() => client.query(`CREATE TABLE tbl_items(
-                    item_id serial,
-                    item_type_id integer,
-                    item_store_id integer,
-                    item_condition_id integer,
-                    item_name varchar(20),
-                    item_cost money,
-                    item_sale_price money,
-                    item_mrsp money,
-                    item_stock_quantity smallint,
-                    item_description varchar(120)
-                );`))
+        member_id serial PRIMARY KEY,
+        member_password varchar(32),
+        preffered_store integer,
+        member_first_name varchar(20),
+        member_last_name varchar(30),
+        member_postal_code varchar(6),
+        member_phone varchar(10),
+        member_email varchar(60),
+        member_mailing_list boolean
+        );`)
+
+        .then(() => client.query(`CREATE TABLE tbl_items(
+            item_id serial PRIMARY KEY,
+            item_type_id integer,
+            store_id integer,
+            condition_id integer,
+            item_name varchar(20),
+            item_cost money,
+            item_sale_price money,
+            item_mrsp money,
+            item_stock_quantity smallint,
+            item_description varchar(120)
+            );`))
+        /* .then(() => client.query(`CREATE TABLE tbl_items(
+            item_id serial,
+            item_type_id integer,
+            item_store_id integer,
+            item_condition_id integer,
+            item_name varchar(20),
+            item_cost money,
+            item_sale_price money,
+            item_mrsp money,
+            item_stock_quantity smallint,
+            item_description varchar(120)
+        );`)) */
                 .then(() => client.end());
 }
 
@@ -94,6 +108,18 @@ function randPhoneNumber() {
     return (1000000000 + randInt(8999999999)).toString();
 }
 
+function randomWage() {
+    // random wage generation
+    return (Math.random() * 30).toFixed(2);
+
+}
+function randomAvailability() {
+    // random wage generation
+    let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    return randNth(days) + ' ' + randNth(days) + '' + randNth(days);
+
+}
+
 function genMember() {
     let member = [];
 
@@ -106,7 +132,7 @@ function genMember() {
     member.push(randPhoneNumber());
     member.push(`${member[2]}.${member[3]}@example.com`);
     member.push(randNth(['true', 'false']));
-
+    
     return member;
 }
 
@@ -125,11 +151,29 @@ function genItem() {
 
     return item;
 }
+function genEmployee() {
+    let employee = [];
+    // Hardcode password so we can log-in as employees for testing.
+    employee.push('password');
+    employee.push(randNth(firstNames));
+    employee.push(randNth(lastNames));
+    employee.push(randNth(['technician', 'manager']));
+    employee.push(randPhoneNumber());
+    employee.push(`${member[2]}.${member[3]}@example.com`);
+    employee.push(randPhoneNumber());
+    employee.push(randPostalCode());
+    employee.push(randomAvailability());
+    employee.push(randomWage());
+    return employee;
+
+}
 
 function seedTables() {
     // TODO: Move sql to its own file.
     let insertMemberSql = 'INSERT INTO tbl_members(member_password, member_preffered_store, member_first_name, member_last_name, member_postal_code, member_phone, member_email, member_mailing_list) VALUES($1, $2, $3, $4, $5, $6, $7, $8);';
     let insertItemSql = 'INSERT INTO tbl_items(item_type_id, item_store_id, item_condition_id, item_name, item_cost, item_sale_price, item_mrsp, item_stock_quantity, item_description) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);';
+    let insertEmployeeSQL = 'INSERT INTO tbl_employees(employee_password, employee_first_name, employee_last_name, employee_job_title, employee_phone, employee_email, employee_address, employee_postal_code, employee_availability, employee_wage) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);';
+
 
     let client = new Client({
         user: 'pressstartadmin',
@@ -146,6 +190,10 @@ function seedTables() {
     let items = Array.from({length: 50}, genItem);
     for (const item of items) {
         queries = queries.then(() => client.query(insertItemSql, item));
+    }
+    let items = Array.from({length: 10}, genMember);
+    for (const item of items) {
+        queries = queries.then(() => client.query(insertEmployeeSQL, employee));
     }
 
     queries.then(() => client.end());
