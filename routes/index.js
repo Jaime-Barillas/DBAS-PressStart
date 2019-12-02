529a2746700bce240dc7c09350171a385b18e044
Jaime Barillas <2942256-JBarillas@users.noreply.gitlab.com>
2019-12-01 22:48:16 -0500

    Setup report

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../db/api.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/monthlyreport', function(req, res, next) {
+    let reportPromise = db.Reports.itemsReportData();
+    reportPromise.then(data => {
+        let reportData = data.slice(0, 6);
+        res.render('index', {title: JSON.stringify(reportData)});
+    });
 });
 
 module.exports = router;
