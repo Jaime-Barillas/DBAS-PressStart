3e0919895c37f1f1c006e6a11fafd3b843d36ad8
Jaime Barillas <2942256-JBarillas@users.noreply.gitlab.com>
2019-12-01 22:48:16 -0500

    Setup report

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./db/api.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reports', reportRouter);
+
+db.initialize();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
