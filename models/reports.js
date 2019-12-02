3e7cea6983bceae41479593cfa71f3e21fb4681e
Shaun McCrum <shaun.k.mccrum@gmail.com>
2019-12-01 23:54:21 -0500

    disabled broken report rout temporarily.

--- models/reports.js
+++ models/reports.js
@@ -2,16 +2,16 @@ var api = require('../db/api');
 
 var Report = api.Reports;
 
-var monthlyReport = Report.monthlyReport();
-monthlyReport.then(items => console.log(items));
+// var monthlyReport = Report.monthlyReport();
+// monthlyReport.then(items => console.log(items));
 
-// Virtual for schema URL
-monthlyReport
-  .virtual('url')
-  .get(function () {
-    console.log("Report line item")
-    return '/views/reports' + this._id;
-  });
+// // Virtual for schema URL
+// monthlyReport
+//   .virtual('url')
+//   .get(function () {
+//     console.log("Report line item")
+//     return '/views/reports' + this._id;
+//   });
   
-//Export model
-module.exports = api.Reports('MonthlyQuery', monthlyReport);
+// //Export model
+// module.exports = api.Reports('MonthlyQuery', monthlyReport);
